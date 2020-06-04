a44d582db0ef34488e4978275eb34d199f0f67ef
Korolev Dmitry <startking@mail.ru>
2020-06-05 00:51:51 +0300

    add timer iterface
    add timer interface

--- DSRPractice/main.c
+++ DSRPractice/main.c
@@ -3,9 +3,14 @@
 #include <stm32f4xx_rcc.h> 
 #include <stm32f4xx_tim.h> 
 
-
+TIM_OCInitTypeDef oc_init; 
 void initLeds(void)
 {
+    TIM_OCStructInit(&oc_init);
+    oc_init.TIM_OCMode = TIM_OCMode_PWM1;  
+    oc_init.TIM_OutputState = TIM_OutputState_Enable;
+    oc_init.TIM_OCPolarity = TIM_OCPolarity_High; 
+
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
     GPIO_InitTypeDef GPIO_InitStructure;
@@ -16,57 +21,86 @@ void initLeds(void)
     GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
 
 
+
+    // Add led to timer out
     GPIO_Init(GPIOD, &GPIO_InitStructure);
     GPIO_PinAFConfig(GPIOD,GPIO_PinSource12,GPIO_AF_TIM4); 
     GPIO_PinAFConfig(GPIOD,GPIO_PinSource13,GPIO_AF_TIM4); 
     GPIO_PinAFConfig(GPIOD,GPIO_PinSource14,GPIO_AF_TIM4);
     GPIO_PinAFConfig(GPIOD,GPIO_PinSource15,GPIO_AF_TIM4);
-
-
 }
 
 void initTimer()
 {
-        //                 TIMER
+    //      TIMER
     TIM_TimeBaseInitTypeDef base_timer; 
     TIM_TimeBaseStructInit(&base_timer); 
-
-    base_timer.TIM_Prescaler = 8400 - 1;   // делитель частоты 
-    base_timer.TIM_Period = 10000;  // период
-    base_timer.TIM_CounterMode = TIM_CounterMode_Up; // счёт вверх 
-    TIM_TimeBaseInit(TIM4, &base_timer); 
-
-    TIM_OCInitTypeDef oc_init; 
-    TIM_OCStructInit(&oc_init);
-    oc_init.TIM_OCMode = TIM_OCMode_PWM1;   // работаем в режиме ШИМ ( PWM ) 
-    oc_init.TIM_OutputState = TIM_OutputState_Enable;
-    oc_init.TIM_OCPolarity = TIM_OCPolarity_High;  // положительная полярность 
-
-
-    // 1  
-    oc_init.TIM_Pulse = 500;   // частота шим 
-    TIM_OC1Init(TIM4,&oc_init);   /// заносим данные в первый канал - порт D12 
-    TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable); 
-    // 2 
-    oc_init.TIM_Pulse = 50000; /// изменяем частоту шим 
-    TIM_OC2Init(TIM4,&oc_init);  // настраиваем второй канал D13 
-    TIM_OC2PreloadConfig(TIM4, TIM_OCPreload_Enable); 
-    // 3 канал - D14 
-    oc_init.TIM_Pulse = 500; 
-    TIM_OC3Init(TIM4,&oc_init); 
-    TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable); 
-    // 4  - D15 
-    oc_init.TIM_Pulse = 5000; 
-    TIM_OC4Init(TIM4,&oc_init);
-    TIM_OC4PreloadConfig(TIM4, TIM_OCPreload_Enable); 
+    base_timer.TIM_Prescaler = 0;
+    base_timer.TIM_Period = 1000; 
+    base_timer.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInit(TIM4, &base_timer);
     TIM_ARRPreloadConfig(TIM4,ENABLE); 
-    TIM_Cmd(TIM4,ENABLE);   // запускаем счёт
+    TIM_Cmd(TIM4,ENABLE);
 }
 
-int main(void){ 
+void setRed(int red)
+{
+    oc_init.TIM_Pulse = red; 
+    TIM_OC3Init(TIM4,&oc_init); 
+    TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable); 
+}
+
+void setGreen(int green)
+{
+    oc_init.TIM_Pulse = green;
+    TIM_OC1Init(TIM4, &oc_init);
+    TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable); 
+}
+
+void setOrange(int orange)
+{
+    oc_init.TIM_Pulse = orange; 
+    TIM_OC2Init(TIM4,&oc_init); 
+    TIM_OC2PreloadConfig(TIM4, TIM_OCPreload_Enable); 
+}
+
+void setBlue(int blue)
+{
+     oc_init.TIM_Pulse = blue; 
+    TIM_OC4Init(TIM4,&oc_init);
+    TIM_OC4PreloadConfig(TIM4, TIM_OCPreload_Enable); 
+
+}
+
+
+void delay(uint32_t milliseconds) {
+  uint32_t start = 0;
+ for(int i =0; i < milliseconds; i++){};
+}
+int main(void){
     initLeds(); 
-    initTimer(); 
-    while (1){ 
+    initTimer();
+ 
+    while (1)
+    {
+        int i;
+        for( i = 0; i<1000; i++)
+        {
+                setRed(i);
+                setBlue(i);
+                setGreen(1000 - i);
+                setOrange(1000 - i);
+                delay(10000);
+        }
+
+        for(; i>0; i--)
+        {
+                setRed(i);
+                setBlue(i);
+                setGreen(1000 - i);
+                setOrange(1000 - i);
+                delay(10000);
+        }
     } 
     return 0; 
 }
